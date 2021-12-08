25b1b08869e8f1a7a4e2dffee1696f021c1f6077
SovaV <sova.volodymyr95@gmail.com>
2021-12-08 18:03:24 +0200

    finish

--- contacts.js
+++ contacts.js
@@ -1,29 +1,60 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-const contactsPath = path.resolve("./db/contacts.json");
+// const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 // TODO: задокументировать каждую функцию
-function listContacts() {
-  fs.readFile(contactsPath, "utf8", (error, data) => {
-    if (error) {
-      console.error(error);
-    }
-    console.log(data);
-    console.log(JSON.parse(data));
-  });
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    console.table(JSON.parse(data.toString()));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data.toString());
+    const getContact = contacts.filter(({ id }) => id === contactId);
+    console.table(getContact);
+    return getContact;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data.toString());
+    const deletedContact = contacts.filter(({ id }) => id !== contactId);
+    console.table(deletedContact);
+    await fs.writeFile(contactsPath, JSON.stringify(deletedContact, null, 2));
+    return;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const newContact = {
+      id: JSON.parse(data.toString()).length + 1,
+      name,
+      email,
+      phone,
+    };
+    const contacts = JSON.parse(data.toString()).concat(newContact);
+    console.table(contacts);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf8");
+    return;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,26 +1,67 @@
-const contacts = require("./contacts");
-console.log(contacts);
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
 
-const argv = require("yargs").argv;
+// const argv = require("yargs").argv;
 
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       listContacts();
+//       break;
+
+//     case "get":
+//       getContactById(id);
+//       break;
+
+//     case "remove":
+//       removeContact(id);
+//       break;
+
+//     case "add":
+//       addContact(name, email, phone);
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
+
+// invokeAction(argv);
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      contacts.listContacts();
+      listContacts();
       break;
 
     case "get":
-      contacts.getContactById(id);
-      break;
-
-    case "add":
-      contacts.removeContact(name, email, phone);
-
+      getContactById(id);
       break;
 
     case "remove":
-      contacts.addContact(id);
+      removeContact(id);
+      break;
 
+    case "add":
+      addContact(name, email, phone);
       break;
 
     default:
